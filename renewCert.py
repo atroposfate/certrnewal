e907cbdccc585ec361df3e81e608e04e2b551d58
atroposfate <41923482+atroposfate@users.noreply.github.com>
2023-04-14 11:22:17 -0600

    Create renewCert.py
    This hasn't been tested yet but is pretty basic

---
+++ renewCert.py
@@ -0,0 +1,25 @@
+#!/bin/python
+
+from subprocess import run
+import os
+
+#Script must be run as root
+if os.geteuid() != 0:
+   print("This script must be run as root please retry with sudo in front")
+   quit()
+
+print("/nUpdating certificates and restarting necessary systems")
+print("Stopping Nginx")
+subprocess.run(['systemctl','stop','nginx'])
+print("done")
+print("Renwing Certs")
+errorchek = subprocess.run(['certbot','renew'], capture_output=True)
+if errorchek.returncode() != 0:
+  print("error on cert renew")
+  print("stdout: ", errorchek.stdout.decode())
+  quit()
+print("done")
+print("Restart necessary systems including Nginx")
+subprocess.run(['systemctl','start','nginx'])
+subprocess.run(['systemctl','restart','postfix','dovecot'])
+print("Done!, all updates have been completed and should be working")
